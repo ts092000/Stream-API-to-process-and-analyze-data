432d06521a42d271c100093f126a9d8c9fd0f258
ts092000 <ts092000@gmail.com>
2022-04-26 13:08:43 +0700

    Chapter 7: Collecting Data from Streams to Create Lists and Sets

--- ComputeStatistics.java
+++ ComputeStatistics.java
@@ -6,32 +6,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ComputeStatistics {
     public static void main(String[] args) {
 
-        String lineForNewYork = "1;New York; New York;8 336 817;780,9";
-        ToDoubleFunction<String> lineToDensity = line -> {
-            String[] split = line.split(";");
+//        String lineForNewYork = "1;New York; New York;8 336 817;780,9";
+//        ToDoubleFunction<String> lineToDensity = line -> {
+//            String[] split = line.split(";");
+//
+//            String populationAsString = split[3];
+//            populationAsString = populationAsString.replace(" ", "");
+//            int population = Integer.parseInt(populationAsString);
+//
+//            String landAreaAsString = split[4];
+//            landAreaAsString = landAreaAsString.replace(" ", "").replace(',', '.');
+//            double landArea = Double.parseDouble(landAreaAsString);
+//
+//            return population / landArea;
+//        };
 
-            String populationAsString = split[3];
-            populationAsString = populationAsString.replace(" ", "");
-            int population = Integer.parseInt(populationAsString);
-
-            String landAreaAsString = split[4];
-            landAreaAsString = landAreaAsString.replace(" ", "").replace(',', '.');
-            double landArea = Double.parseDouble(landAreaAsString);
-
-            return population / landArea;
-        };
-
-        double density = lineToDensity.applyAsDouble(lineForNewYork);
-        System.out.println("Density of New York = " + density);
+//        double density = lineToDensity.applyAsDouble(lineForNewYork);
+//        System.out.println("Density of New York = " + density);
+        Function<String, String> lineToName =
+                line -> line.split(";")[1];
 
         Path path = Path.of("data/cities.csv");
+        Set<String> cities = null;
         try (Stream<String> lines = Files.lines(path, StandardCharsets.ISO_8859_1);) {
 //            double min = lines.skip(2)
 //                    .map(lineToDensity::apply)
@@ -43,16 +49,40 @@ public class ComputeStatistics {
 //                    .max()
 //                    .orElseThrow();
 
-            DoubleSummaryStatistics summaryStatistics = lines.skip(2)
-                    .mapToDouble(lineToDensity)
-                    .summaryStatistics();
+//            DoubleSummaryStatistics summaryStatistics = lines.skip(2)
+//                    .mapToDouble(lineToDensity)
+//                    .summaryStatistics();
 
 //            System.out.println("Min = " + min);
 //            System.out.println("Max = " + max);
-            System.out.println("Stats = " + summaryStatistics);
+//            System.out.println("Stats = " + summaryStatistics);
+
+            cities = lines.skip(2)
+                    .map(lineToName)
+                    .collect(Collectors.toSet());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println("# cities = " + cities.size());
+
+        List<String> citiesWithA = cities.stream()
+                .filter(city -> city.startsWith("A"))
+                .collect(Collectors.toList());
+        System.out.println("Cities with 'A': " + citiesWithA);
+
+//        String[] array = cities.toArray(String[]::new);
+        String[] array = cities.stream().toArray(String[]::new);
+        System.out.println("# array = " + array.length);
+
+        String joined = cities.stream()
+                .filter(name -> name.length() == 4)
+                .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(joined);
+
+        String collect = Stream.<String>empty()
+                .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println("Collecting an empty stream: " + collect);
     }
 }
