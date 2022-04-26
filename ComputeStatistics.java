db35206adb3386e28a8a8352f66bdca125d6c075
ts092000 <ts092000@gmail.com>
2022-04-26 14:29:45 +0700

    Chapter 8: Creating and Analyzing Histograms from Streams

--- ComputeStatistics.java
+++ ComputeStatistics.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import com.bkitsolution.City;
 
 public class ComputeStatistics {
     public static void main(String[] args) {
@@ -33,12 +32,30 @@ public class ComputeStatistics {
 
 //        double density = lineToDensity.applyAsDouble(lineForNewYork);
 //        System.out.println("Density of New York = " + density);
-        Function<String, String> lineToName =
-                line -> line.split(";")[1];
+//        Function<String, String> lineToName =
+//                line -> line.split(";")[1];
+        Function<String, City> lineToCity = line -> {
+            String[] split = line.split(";");
+
+            String cityName = split[1].trim();
+
+            String state = split[2].trim();
+
+            String populationAsString = split[3];
+            populationAsString = populationAsString.replace(" ", "");
+            int population = Integer.parseInt(populationAsString);
+
+            String landAreaAsString = split[4];
+            landAreaAsString = landAreaAsString.replace(" ", "").replace(',', '.');
+            double landArea = Double.parseDouble(landAreaAsString);
+
+            return new City(cityName, state, population, landArea);
+        };
 
         Path path = Path.of("data/cities.csv");
-        Set<String> cities = null;
+        Set<City> cities = null;
         try (Stream<String> lines = Files.lines(path, StandardCharsets.ISO_8859_1);) {
+
 //            double min = lines.skip(2)
 //                    .map(lineToDensity::apply)
 //                    .min(Comparator.naturalOrder())
@@ -57,32 +74,80 @@ public class ComputeStatistics {
 //            System.out.println("Max = " + max);
 //            System.out.println("Stats = " + summaryStatistics);
 
+//            cities = lines.skip(2)
+//                    .map(lineToName)
+//                    .collect(Collectors.toSet());
+
             cities = lines.skip(2)
-                    .map(lineToName)
+                    .map(lineToCity)
                     .collect(Collectors.toSet());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("# cities = " + cities.size());
-
-        List<String> citiesWithA = cities.stream()
-                .filter(city -> city.startsWith("A"))
-                .collect(Collectors.toList());
-        System.out.println("Cities with 'A': " + citiesWithA);
+//        System.out.println("# cities = " + cities.size());
+//
+//        List<String> citiesWithA = cities.stream()
+//                .filter(city -> city.startsWith("A"))
+//                .collect(Collectors.toList());
+//        System.out.println("Cities with 'A': " + citiesWithA);
 
 //        String[] array = cities.toArray(String[]::new);
-        String[] array = cities.stream().toArray(String[]::new);
-        System.out.println("# array = " + array.length);
+//        String[] array = cities.stream().toArray(String[]::new);
+//        System.out.println("# array = " + array.length);
+//
+//        String joined = cities.stream()
+//                .filter(name -> name.length() == 4)
+//                .collect(Collectors.joining(", ", "[", "]"));
+//        System.out.println(joined);
+//
+//        String collect = Stream.<String>empty()
+//                .collect(Collectors.joining(", ", "[", "]"));
+//        System.out.println("Collecting an empty stream: " + collect);
 
-        String joined = cities.stream()
-                .filter(name -> name.length() == 4)
-                .collect(Collectors.joining(", ", "[", "]"));
-        System.out.println(joined);
+        System.out.println("# cities = " + cities.size());
 
-        String collect = Stream.<String>empty()
-                .collect(Collectors.joining(", ", "[", "]"));
-        System.out.println("Collecting an empty stream: " + collect);
+        Map<String, List<City>> citiesPerState =
+                cities.stream().collect(Collectors.groupingBy(city -> city.getState()));
+
+        System.out.println("Map size = " + citiesPerState.size());
+
+        List<City> citiesOfUtah = citiesPerState.get("Utah");
+        System.out.println(citiesOfUtah);
+
+        Map<String, Long> numberOfCitiesPerState =
+                cities.stream()
+                .collect(Collectors.groupingBy(city -> city.getState(), Collectors.counting()));
+
+        System.out.println("# cities in Utah: " + numberOfCitiesPerState.get("Utah"));
+
+        Map.Entry<String, Long> stateWithMostCites = numberOfCitiesPerState
+                .entrySet().stream()//Stream Map.Entry<String, Long>
+//                .max(Comparator.comparing(Map.Entry::getValue))
+                .max(Map.Entry.comparingByValue()).orElseThrow();
+
+        System.out.println("State with most cities: " + stateWithMostCites);
+
+//        int populationOfUtah = citiesPerState.get("Utah").stream()
+//                .mapToInt(city -> city.getPopulation())
+//                .sum();
+//                .collect(Collectors.summingInt(city -> city.getPopulation()));
+//        System.out.println(populationOfUtah);
+
+        Map<String, Integer> populationOfCitiesPerState =
+                cities.stream()
+                        .collect(Collectors.groupingBy(city -> city.getState(),
+                                Collectors.summingInt(city -> city.getPopulation())));
+
+        System.out.println("Population of Utah = " + populationOfCitiesPerState.get("Utah"));
+
+        Map.Entry<String, Integer> stateWithTheMostPeople =
+                populationOfCitiesPerState.entrySet().stream() // Stream Map.Entry<String, Integer>
+                        // .max(Comparator.comparing(Entry::getValue))
+                        .max(Map.Entry.comparingByValue())
+                        .orElseThrow();
+
+        System.out.println(stateWithTheMostPeople);
     }
 }
